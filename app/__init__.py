c9005535b05201e7a4066c111c782f10c7f1131d
Muntasir Abdullah Bin Ahmed <92276703+Muntasirzx@users.noreply.github.com>
2025-07-14 07:59:27 +0600

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,11 @@
+# app/__init__.py
+
+from flask import Flask
+
+# Create the main Flask application instance
+app = Flask(__name__)
+
+# Import the routes to register them with the Flask app
+# This is placed at the bottom to avoid circular import errors.
+from app import routes
 
